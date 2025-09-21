288735e6d276a6ec32a040047cc7e2dee32d06fc
TYleRW83811 <swirl_09_sours@icloud.com>
2025-09-21 08:03:14 +0100

    Setup 排序算法实现 using MAUI

---
+++ SortingAlgorithmApp_0921_1503_rre.cs
@@ -0,0 +1,10 @@
+// 代码生成时间: 2025-09-21 15:03:13
+ * It provides a simple console-based application to sort a list of integers.
+ *
+ * Features:
+ * - Bubble Sort
+ * - Selection Sort
+ * - Insertion Sort
+ *
+ * The application allows users to select a sorting algorithm and then sort a list of integers.
+ *
